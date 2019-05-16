0d54655e3f40e9744d965db9790cff1cbc133ee9
DayanaD <50094128+DayanaD@users.noreply.github.com>
2019-05-15 22:18:57 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DayannaDelgadoLopez3A.Clases;
+using DayannaDelgadoLopez3A.Interfaz ;
+
+namespace DayannaDelgadoLopez3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new cliente( "Delgado López" , "Dayanna María" , 1316876453 , "Tarqui" ). MostrarDatos();
+            new gasolina( " Extra " , 5 , 2.0 ). MostrarDatos();
+            new gasolina( " Extra " , 5 , 2.0 ). HacerCalculo();
+            Console.ReadKey();
+        }
+    }
+}
